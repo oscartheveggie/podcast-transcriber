a8695de94b43b0fc85a17a0a907cd609624e85b3
oscartheveggie <oscartsoi2017@gmail.com>
2026-06-18 16:17:03 +0800

    finished tester and added whisper and sensevoice

--- src/podcast_transcriber/tester.py
+++ src/podcast_transcriber/tester.py
@@ -10,6 +10,8 @@ import time
 import transcript
 from transcript import Transcriber
 from tqdm import tqdm
+from mutagen.mp3 import MP3
+from datetime import datetime
 
 
 class Tester:
@@ -20,7 +22,7 @@ class Tester:
                  model_dir: str,
                  output_dir: str,
                  device: str = "cpu",
-                 compute_size: str = "int8",
+                 compute_type: str = "int8",
                  batch_size: int = 16,
                  diarize: bool = False,
                  save_output: bool = True
@@ -28,7 +30,7 @@ class Tester:
 
         if models is None or len(models) == 0:
             raise ValueError("At least one model must be specified for testing.")
-        if models not in transcript.AVAILABLE_MODELS:
+        if not set(models).issubset(set(transcript.AVAILABLE_MODELS)):
             raise ValueError(f"One or more specified models are not supported. Available models: {transcript.AVAILABLE_MODELS}")
 
         self.audio_path: str = audio_path
@@ -36,25 +38,60 @@ class Tester:
         self.model_dir: str = model_dir
         self.save_output: bool = save_output
         self.output_dir: str = output_dir
+        self.device = device
+        self.compute_type = compute_type
+        self.batch_size = batch_size
+        self.diarize = diarize
         self.results: dict[str, dict] = {} # model_name -> {transcript: str, processing_time: float}
+
+    def _save_output(self):
+
+        if not self.save_output:
+            return
+
+        with open(self.output_dir / f"test_results_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.txt", "w", encoding="utf-8") as f:
+
+            f.write(f"===== Model Test Results =====\n")
+            f.write(f"Audio file: {self.audio_path} (duration: {self.get_audio_duration():.2f} seconds)\n")
+            f.write(f"Models tested: {self.models}\n\n")
+
+            for model_name, result in self.results.items():
+                f.write(f"Model: {model_name}\n")
+                f.write(f"Processing time: {result['processing_time']:.2f} seconds\n")
+                f.write("\n")
+            
+            f.write(f"\n===== Transcript Output =====\n\n")
+            
+            for model_name, result in self.results.items():
+                f.write(f"Model: {model_name}\n")
+                f.write(f"Transcript:\n{result['transcript']}\n")
+                f.write("\n")
 
     def run_tests(self):
 
+        print("="*50)
+        print("TRANSCRIPTION MODEL TESTER")
+        print("="*50)
+        
+        print(f"\nAudio file: {self.audio_path} (duration: {self.get_audio_duration():.2f} seconds)")
+        print(f"Models to test: {self.models}")
+        
         for i, model_name in enumerate(self.models):
             print(f"\nTesting model: {model_name} ({i+1}/{len(self.models)})")
             transcriber = Transcriber(model_name=model_name, 
                                       model_dir=self.model_dir,
                                       device=self.device,
-                                      compute_size=self.compute_size,
+                                      compute_type=self.compute_type,
                                       batch_size=self.batch_size,
                                       diarize=self.diarize
                                       )
             start_time = time.time()
-            transcript = tqdm(transcriber.transcribe(self.audio_path))
+            transcript = transcriber.transcribe(self.audio_path)
             end_time = time.time()
             processing_time = end_time - start_time
-
             print(f"Processing time for {model_name}: {processing_time:.2f} seconds")
+
+            transcript = transcript["text"] if "text" in transcript else transcript["segments"][0]["text"] if "segments" in transcript and len(transcript["segments"]) > 0 else ""
 
             self.results[model_name] = {
                 "transcript": transcript,
@@ -64,28 +101,20 @@ class Tester:
         print(f"\nAll tests completed. Results:")
         print({model: {"processing_time": result["processing_time"]} for model, result in self.results.items()})
 
-        if self.save_output:
-            for model_name, result in self.results.items():
-                with open(self.output_dir / f"transcript_{model_name}.txt", "w", encoding="utf-8") as f:
-                    f.write(result["transcript"])
-            
-            with open(self.output_dir / "test_results.txt", "w", encoding="utf-8") as f:
-                for model_name, result in self.results.items():
-                    f.write(f"Model: {model_name}\n")
-                    f.write(f"Processing time: {result['processing_time']:.2f} seconds\n")
-                    f.write("\n")
-            
+        self._save_output()
+    
+    def get_audio_duration(self) -> float:
+        """Get the duration of the audio file in seconds."""
+        audio = MP3(self.audio_path)
+        return audio.info.length
+
 
 if __name__ == "__main__":
     tester = Tester(
-        audio_path="input/transcoded.mp3",
-        models=["whisperx_small", "whisperx_medium"],
-        model_dir="model",
-        output_dir="output",
-        device="cpu",
-        compute_size="int8",
-        batch_size=16,
-        diarize=False,
+        audio_path="input/cantonese.mp3",
+        models=["whisperx_small", "whisper_small", "sensevoice_small"],
+        model_dir=transcript.SCRIPT_DIR / "../../model",
+        output_dir=transcript.SCRIPT_DIR / "../../output",
         save_output=True
     )
     tester.run_tests()
--- src/podcast_transcriber/transcript.py
+++ src/podcast_transcriber/transcript.py
@@ -1,6 +1,7 @@
 """Transcript helpers."""
 
 from pathlib import Path
+from pyannote import audio
 import whisperx
 import gc
 from whisperx.diarize import DiarizationPipeline
@@ -8,13 +9,29 @@ import torch
 from tqdm import tqdm
 import os
 import dotenv
+import whisper
+from funasr import AutoModel
+from funasr.utils.postprocess_utils import rich_transcription_postprocess
+from qwen_asr import Qwen3ASRModel
+
 
 dotenv.load_dotenv()
 HF_TOKEN = os.getenv("HF_TOKEN") # get from https://huggingface.co/settings/tokens
 
 SCRIPT_DIR = Path(__file__).parent
 
-AVAILABLE_MODELS = ["whisperx_small", "whisperx_medium"] # for now, only whisperx is supported
+AVAILABLE_MODELS = ["whisperx_small", 
+                    "whisperx_medium", 
+                    "whisper_small", 
+                    "whisper_medium",
+                    "sensevoice_small",
+                    "sensevoice_large",
+                    "qwen3_asr",]
+
+NUM_CORES = os.cpu_count() or 1
+os.environ["OMP_NUM_THREADS"] = str(NUM_CORES) # num of cores used
+os.environ["MKL_NUM_THREADS"] = str(NUM_CORES) # num of cores used
+
 
 class Transcriber:
 
@@ -23,7 +40,8 @@ class Transcriber:
                  device: str = "cpu", 
                  compute_type: str = "int8",
                  batch_size: int = 16,
-                 diarize: bool = False):
+                 diarize: bool = False,
+                 align: bool = False):
         
         self.model_name: str = model_name
         if self.model_name not in AVAILABLE_MODELS:
@@ -34,8 +52,9 @@ class Transcriber:
         self.compute_type: str = compute_type
         self.batch_size: int = batch_size
         self.diarize: bool = diarize
-        self.transcript: dict | None = None
+        self.align: bool = align
 
+        self.transcript: dict | None = None
         self.model = None
     
 
@@ -55,10 +74,11 @@ class Transcriber:
         print(result["segments"]) # before alignment
 
         # 2. Align whisper output
-        print("\n" + "="*50 + "\nAligning whisper output...")
-        model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=self.device)
-        result = whisperx.align(result["segments"], model_a, metadata, audio, self.device, return_char_alignments=False)
-        print(result["segments"]) # after alignment
+        if self.align:
+            print("\n" + "="*50 + "\nAligning whisper output...")
+            model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=self.device)
+            result = whisperx.align(result["segments"], model_a, metadata, audio, self.device, return_char_alignments=False)
+            print(result["segments"]) # after alignment
 
         # 3. Assign speaker labels
         if (self.diarize):
@@ -77,7 +97,89 @@ class Transcriber:
         self.transcript = result
 
         return self.transcript
+
+    def _transcribe_whisper(self, model_size: str, audio_path: str | Path) -> dict:
+        """Transcribe the given audio file and return the transcript text."""
+        
+        # 1. Transcribe with whisper
+        self.model: whisper.Whisper = whisper.load_model(model_size, device=self.device)
+
+        print("\n" + "="*50 + "\nTranscribing audio with Whisper...")
+        result = self.model.transcribe(str(audio_path))
+        print(result["segments"])
+
+        # 2. Align whisper output
+        if self.align:
+            print("\n" + "="*50 + "\nAligning whisper output...")
+            audio = whisperx.load_audio(str(audio_path))
+            model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=self.device)
+            cleaned_result = [{"text": segment["text"], "start": segment["start"], "end": segment["end"]} 
+                              for segment in result["segments"] if segment["text"].strip() != ""] # remove empty segments and keep only text, start and end
+            result = whisperx.align(cleaned_result, model_a, metadata, audio, self.device, return_char_alignments=False)
+            print(result["segments"]) # after alignment
+
+        self.transcript = result
+
+        return self.transcript
     
+    def _transcribe_sensevoice(self, model_size: str, audio_path: str | Path) -> dict:
+        """Transcribe the given audio file and return the transcript text."""
+
+        model_dir = "iic/SenseVoiceSmall" if model_size == "small" else "iic/SenseVoiceLarge"
+        model = AutoModel(
+            model=model_dir,
+            trust_remote_code=True,
+            remote_code="./model.py",    
+            vad_model="fsmn-vad",
+            vad_kwargs={"max_single_segment_time": 30000},
+            device=self.device,
+        )
+
+        res = model.generate(
+            input=str(audio_path),
+            cache={},
+            language="auto",  # "zh", "en", "yue", "ja", "ko", "nospeech"
+            use_itn=True,
+            batch_size_s=60,
+            merge_vad=True,
+            merge_length_s=15,
+        )
+
+        # post-process the raw transcription results
+        self.transcript = {"text": rich_transcription_postprocess(res[0]["text"])}
+    
+        print(self.transcript)
+        return self.transcript
+
+    def _transcribe_qwen3_asr(self, audio_path: str | Path) -> dict:
+        """Transcribe the given audio file and return the transcript text."""
+
+        model = Qwen3ASRModel.from_pretrained(
+            "Qwen/Qwen3-ASR-0.6B",
+            dtype=torch.bfloat16,
+            device_map="auto",
+            # attn_implementation="flash_attention_2",
+            max_inference_batch_size=32, # Batch size limit for inference. -1 means unlimited. Smaller values can help avoid OOM.
+            max_new_tokens=256, # Maximum number of tokens to generate. Set a larger value for long audio input.
+            forced_aligner="Qwen/Qwen3-ForcedAligner-0.6B",
+            forced_aligner_kwargs=dict(
+                dtype=torch.bfloat16,
+                device_map="auto",
+                # attn_implementation="flash_attention_2",
+            ),
+        )
+
+        results = model.transcribe(
+            audio=[str(audio_path)],
+            language=None, # can also be set to None for automatic language detection
+            return_time_stamps=True,
+        )
+
+        print(results)
+
+        self.transcript = results[0] # for simplicity, we only handle single audio input here
+        return self.transcript
+
 
     def transcribe(self, audio_path: str | Path, min_speakers: int = None, max_speakers: int = None) -> dict:
 
@@ -94,6 +196,19 @@ class Transcriber:
                                              min_speakers=min_speakers, 
                                              max_speakers=max_speakers)
         
+        # 2. Whisper model
+        if self.model_name.startswith("whisper"):
+            model_size = self.model_name.split("_")[1] # e.g. "small" or "medium"
+            return self._transcribe_whisper(model_size=model_size, audio_path=audio_path)
+
+        # 3. SenseVoice ASR model
+        if self.model_name.startswith("sensevoice"):
+            model_size = self.model_name.split("_")[1] # e.g. "small" or "medium"
+            return self._transcribe_sensevoice(model_size=model_size, audio_path=audio_path)
+
+        if self.model_name.startswith("qwen3_asr"):
+            return self._transcribe_qwen3_asr(audio_path=audio_path)
+
 
     def export_to_txt(self, output_path: str | Path) -> str:
         """Export the transcript text to a .txt file and return the file path."""
@@ -106,6 +221,13 @@ class Transcriber:
         
         with open(output_path, "w", encoding="utf-8") as f:
             print("Writing transcript to file...")
+
+            if self.transcript.get("segments") is None:
+                # if no segments, just write the full text
+                f.write(self.transcript.get("text", ""))
+                print("No segments found in transcript. Wrote full text to file.")
+                return str(output_path)
+
             for segment in tqdm(self.transcript["segments"], desc="Exporting segments"):
                 start_time = segment["start"]
                 end_time = segment["end"]
@@ -117,10 +239,21 @@ class Transcriber:
 
 
 if __name__ == "__main__":
+
+    # AVAILABLE_MODELS = ["whisperx_small",
+    #                     "whisperx_medium", 
+    #                     "whisper_small", 
+    #                     "whisper_medium",
+    #                     "sensevoice_small",
+    #                     "sensevoice_large",
+    #                     "qwen3_asr",]
+
+    model_name = "qwen3_asr"
+    align = True
     
-    transcriber = Transcriber(model_dir=SCRIPT_DIR/"../../model")
-    transcriber.transcribe(audio_path=SCRIPT_DIR / "../../input/transcoded.mp3", min_speakers=1, max_speakers=2)
-    sample_transcript = "This is a sample transcript for testing."
-    output_file = SCRIPT_DIR / "../../output/sample_transcript.txt"
+    transcriber = Transcriber(model_name=model_name, model_dir=SCRIPT_DIR/"../../model", align=align)
+    transcriber.transcribe(audio_path=SCRIPT_DIR / "../../input/debug.mp3")
+    sample_transcript = "This is a sample transcript for debugging."
+    output_file = SCRIPT_DIR / "../../output/debug_transcript.txt"
     saved_path = transcriber.export_to_txt(output_file)
     
